e7eb67f22e8db10058c154130f3e83eda1f18caa
Lucas Amaral <lba4@cin.ufpe.br>
2025-06-15 14:19:44 -0300

    Iniciando a categoria dos produtos

--- src/components/ProductCategory/index.jsx
+++ src/components/ProductCategory/index.jsx
@@ -1,3 +1,4 @@
+import styles from './styles.module.css';
 
 function ProductCategory() {
     const data = {
@@ -5,19 +6,49 @@ function ProductCategory() {
         "title": "Boneca de Pano",
         "price": 85.00,
         "images": [
-            "https://image1.com",
+            "https://www.artesdaceci.com.br/Fotos/Media/Artes_da_Ceci_70-1445799758.jpg",
             "https://image2.com",
             "https://image3.com"
         ],
         "ong": "Instituto Dia Melhor",
-        "description": "dask oaskd oaksod akso dkaso dkaso kda"
+        "description": "Atirei o pau no gato Mas o gato não morreu Dona Chica admirou-se Do berro, do berro que o gato deu Miau! Atirei o pau no gato Mas o gato não morreu Dona Chica admirou-se Do berro, do berro que o gato deu Miau! Miau miau miau Miau miau miau Miau miau miau Miau miau miau Miau miau miau Miau miau miau Miau miau miau"
     }
 
     return (
-        <div>
-            <h2>{data.title}</h2>
-            <p>{data.description}</p>
-            <p>Feito cariosamente por {data.ong}</p>
+        <div className={styles.infos_esquerda}>
+
+            <img src = {data.images[0]}/>
+
+            <div className = {styles.infos_verticais}>
+
+                <h1>
+
+                    <strong>
+
+                        {data.title} 
+
+                    </strong>
+
+                </h1>
+
+                <h2>
+
+                    <strong>
+
+                        R$ {data.price},00
+
+                    </strong>
+
+                </h2>
+
+                <p className={styles.description}>
+
+                    {data.description}
+                    
+                </p>
+
+            </div>
+            
         </div>
     )
 }
