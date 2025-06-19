405d0a1271d58d77dd2b3d339ddf0490c6c6f6f5
Diogo Rodrigues <dsr@cin.ufpe.br>
2025-06-19 20:57:54 -0300

    Commit dos itens básicos
    Implementação da imagem de fundo e textos iniciais (ainda para formatação)

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,31 @@
+import homeBanner from '../../assets/homeBanner/homeBanner.png';
 
-function OngCarousel() {
+
+function HomeBanner() {
     return (
-        <section>
-            Diogo da Silva Rodrigues
-            
+        <section
+        className='h-80'
+        style={{
+        backgroundImage: `url(${homeBanner})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }}
+    >
+        <div className="ml-8 pt-10 text-3xl text-bl text-[#0B236D]">
+            <b>Compre</b> com propósito, <b>transforme</b> vidas.
+        </div>
+
+        <div className="ml-8 text-4xl text-bl text-[#0B236D]">
+            <b>transforme</b> vidas.
+        </div>
+
+        {/* Botão de Saiba Mais */}
+        <button className="bg-[#009FE3] text-white px-10 mt-36 ml-10 py-2 rounded-lg hover:bg-[#2382aa] transition duration-300">
+            Saiba Mais
+        </button>
+
         </section>
-    )
+    );
 }
 
-export default OngCarousel;
+export default HomeBanner;
