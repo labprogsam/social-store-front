c7d8d95d40709f3dbc908669b5312ffca32a5922
SamuelMiranda-neuro <samuel.miranda@neurotech.com.br>
2025-06-29 14:06:04 -0300

    chore: set carousel max width

--- src/components/OngCarousel/index.jsx
+++ src/components/OngCarousel/index.jsx
@@ -1,89 +1,103 @@
-
 // Importando os componentes necessários do biblioteca Swiper responsável por criar o carrossel
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // Importando os assets necessários
-import dividingLine from '../../assets/CarouselMoc/dividingLine.svg';
-import bannerCarousel from '../../assets/CarouselMoc/bannerCarousel.svg';
-import ong1 from '../../assets/CarouselMoc/ong1.svg';
-import ong2 from '../../assets/CarouselMoc/ong2.svg';
-import ong3 from '../../assets/CarouselMoc/ong3.svg';
-import ong4 from '../../assets/CarouselMoc/ong4.svg';
-import ong5 from '../../assets/CarouselMoc/ong5.svg';
-import ong6 from '../../assets/CarouselMoc/ong6.svg';
-import ong7 from '../../assets/CarouselMoc/ong7.svg';
-import ong8 from '../../assets/CarouselMoc/ong8.svg';
+import dividingLine from "../../assets/CarouselMoc/dividingLine.svg";
+import bannerCarousel from "../../assets/CarouselMoc/bannerCarousel.svg";
+import ong1 from "../../assets/CarouselMoc/ong1.svg";
+import ong2 from "../../assets/CarouselMoc/ong2.svg";
+import ong3 from "../../assets/CarouselMoc/ong3.svg";
+import ong4 from "../../assets/CarouselMoc/ong4.svg";
+import ong5 from "../../assets/CarouselMoc/ong5.svg";
+import ong6 from "../../assets/CarouselMoc/ong6.svg";
+import ong7 from "../../assets/CarouselMoc/ong7.svg";
+import ong8 from "../../assets/CarouselMoc/ong8.svg";
 
 // Array dos logos das ONGs
 const ongs = [ong1, ong2, ong3, ong4, ong5, ong6, ong7, ong8];
 
 // Estilos para o slide e as imagens
-const slideStyle = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-const imageStyle = "cursor-pointer h-auto sm:h-32 max-w-[80%] mx-auto py-16"
+const slideStyle =
+  "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4";
+const imageStyle = "cursor-pointer h-auto sm:h-32 max-w-[80%] mx-auto py-16";
 
 // Componente OngCarousel que renderiza o carrossel de ONGs
 function OngCarousel() {
-    return (
-    
+  return (
     <section
-    className="p-14 relative"
-    style={{
+      className="p-14 relative"
+      style={{
         // Definindo o background do carrossel com a imagem importada
         backgroundImage: `url(${bannerCarousel})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-    }}
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
     >
+      {/* Título do carrossel */}
+      <div className="text-center text-4xl font-inter font-bold">
+        Nossas Ongs
+        <img
+          src={dividingLine}
+          alt="Dividing line"
+          className="sm:w-40 mx-auto mt-4"
+        />
+      </div>
 
-        {/* Título do carrossel */}
-        <div className="text-center text-4xl font-inter font-bold">
-            Nossas Ongs
-            <img src={dividingLine} alt="Dividing line" className="sm:w-40 mx-auto mt-4"/>
-        </div>
-        
-        {/* Configurando o Swiper com os módulos de navegação e paginação */}
-        <Swiper
+      {/* Configurando o Swiper com os módulos de navegação e paginação */}
+      <Swiper
         modules={[Navigation, Pagination]}
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
-        >
+        style={{maxWidth: '1400px'}}
+      >
+        {/* Mapeando os logos das ONGs para criar os slides */}
+        <SwiperSlide>
+          <div className={slideStyle}>
+            {ongs.map((logo, index) => (
+              <img
+                key={index}
+                src={logo}
+                alt={`ONG ${index + 1}`}
+                className={imageStyle}
+              />
+            ))}
+          </div>
+        </SwiperSlide>
 
-            {/* Mapeando os logos das ONGs para criar os slides */}
-            <SwiperSlide>
-                <div className={slideStyle}>
-                    {ongs.map((logo, index) => (
-                        <img key={index} src={logo} alt={`ONG ${index + 1}`} className={imageStyle}/>
-                    ))}
-                </div>
-            </SwiperSlide>
+        <SwiperSlide>
+          <div className={slideStyle}>
+            {ongs.map((logo, index) => (
+              <img
+                key={index}
+                src={logo}
+                alt={`ONG ${index + 1}`}
+                className={imageStyle}
+              />
+            ))}
+          </div>
+        </SwiperSlide>
 
-            <SwiperSlide>
-                <div className={slideStyle}>
-                    {ongs.map((logo, index) => (
-                        <img key={index} src={logo} alt={`ONG ${index + 1}`} className={imageStyle}/>
-                    ))}
-                </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-                <div className={slideStyle}>
-                    {ongs.map((logo, index) => (
-                        <img key={index} src={logo} alt={`ONG ${index + 1}`} className={imageStyle}/>
-                    ))}
-                </div>
-            </SwiperSlide>
-
-        </Swiper>
-
+        <SwiperSlide>
+          <div className={slideStyle}>
+            {ongs.map((logo, index) => (
+              <img
+                key={index}
+                src={logo}
+                alt={`ONG ${index + 1}`}
+                className={imageStyle}
+              />
+            ))}
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
-
-    );
+  );
 }
 
 export default OngCarousel;
