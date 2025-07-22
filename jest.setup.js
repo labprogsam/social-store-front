265032fb04e1a060e05a1c6578cda520062154cf
labprogsam <som3@cin.ufpe.br>
2025-07-21 22:09:35 -0300

    feat: create more ui tests

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,18 @@ const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 global.React = React;
+
+class FileReaderMock {
+  constructor() {
+    this.onload = null;
+  }
+
+  readAsDataURL(file) {
+    if (this.onload) {
+      this.result = 'data:image/png;base64,mockbase64data';
+      this.onload();
+    }
+  }
+}
+
+global.FileReader = FileReaderMock;
