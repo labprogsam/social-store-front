1ed8fd9b55aaf295d3cf37726ef62ef1dafb3e9b
labprogsam <som3@cin.ufpe.br>
2025-07-31 17:20:09 -0300

    fix: adjust ong home route

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 
 import productsData from "./ProductList.data";
 import ProductCard from "./ProductCard";
@@ -6,48 +6,47 @@ import TitleBar from "./TitleBar";
 import Button from "./Button";
 
 import {
-    PageContainer,
-    MainContentWrapper,
-    ProductsFlexContainer,
-    ViewMoreButtonContainer
+	PageContainer,
+	MainContentWrapper,
+	ProductsFlexContainer,
+	ViewMoreButtonContainer
 } from './ProductList.styles';
 
 function ProductList() {
 
-    const [selectedProductId, setSelectedProductId] = useState(null);
+	const [selectedProductId, setSelectedProductId] = useState(null);
 
-    const handleCardClick = (id) => {
-        setSelectedProductId(id);
-  };
+	const handleCardClick = (id) => {
+		setSelectedProductId(id);
+	};
 
+	const handleViewMoreClick = () => {
+		alert("Visualizar mais produtos!");
+		// Logica para carregar mais produtos aqui!
+	};
 
-    const handleViewMoreClick = () => {
-        alert("Visualizar mais produtos!");
-        // Logica para carregar mais produtos aqui!
-    };
+	return (
+		<PageContainer>
+			<MainContentWrapper>
+				<TitleBar title="Nossos Produtos" />
 
-    return(
-        <PageContainer>
-            <MainContentWrapper>
-                <TitleBar title="Nossos Produtos" />
+				<ProductsFlexContainer>
+					{productsData.map((product) => (
+						<ProductCard
+							key={product.id}
+							product={product}
+							isSelected={selectedProductId == product.id}
+							onClick={() => handleCardClick(product.id)}
+						/>
+					))}
+				</ProductsFlexContainer>
 
-                <ProductsFlexContainer>
-                    {productsData.map((product) => (
-                        <ProductCard
-                            key={product.id}
-                            product={product}
-                            isSelected={selectedProductId == product.id}
-                            onClick={() => handleCardClick(product.id)}
-                        />
-                    ))}
-                </ProductsFlexContainer>
-                
-                <ViewMoreButtonContainer>
-                    <Button onClick={handleViewMoreClick}>Visualizar mais</Button>
-                </ViewMoreButtonContainer>
-            </MainContentWrapper>
-        </PageContainer>
-    );
+				<ViewMoreButtonContainer>
+					<Button onClick={handleViewMoreClick}>Visualizar mais</Button>
+				</ViewMoreButtonContainer>
+			</MainContentWrapper>
+		</PageContainer>
+	);
 }
 
 export default ProductList;
