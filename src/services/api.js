c1e29acd78331352641c6ccce527634ab3de12c5
Lucas Amaral <lba4@cin.ufpe.br>
2025-08-14 09:37:02 -0300

    ocultando host

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_HOST,
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_SERVER_URL,
 });
 
 export default api;
